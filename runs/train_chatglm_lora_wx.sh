b172e9586c656f820e31cffcf6e8d577357c62da
wufan <fan.wu@richinfoai.com>
2023-05-30 13:35:12 +0800

    新增文件

---
+++ runs/train_chatglm_lora_wx.sh
@@ -0,0 +1,41 @@
+lora_rank=8
+lora_trainable="query_key_value,dense,dense_h_to_4h,dense_4h_to_h"
+modules_to_save="null"
+lora_dropout=0.1
+LR=2e-4
+model_name_or_path="/data/wufan/llm/model/chatglm-6b"   # LLM底座模型路径，或者是huggingface hub上的模型名称
+your_data_path="/data/wufan/data/wx_bilu"  # 填入数据集所在的文件夹路径
+your_checkpopint_path="/data/wufan/llm/experiments/wx_bilu_v1.0.1/"  # 填入用来存储模型的路径
+
+deepspeed_config_file=./configs/deepspeed_config_zero2_offload.json
+source_prefix="请根据下面对话，预测可能继续提问的问题："
+
+torchrun \
+    --nnodes 1 \
+    --nproc_per_node 2 \
+    ft_chatglm_lora/main.py \
+    --deepspeed ${deepspeed_config_file} \
+    --do_train \
+    --train_file $your_data_path/train.json \
+    --validation_file $your_data_path/val.json \
+    --cache_dir $your_data_path/data_cache \
+    --source_prefix ${source_prefix} \
+    --prompt_column input \
+    --response_column target \
+    --overwrite_cache \
+    --model_name_or_path $model_name_or_path \
+    --output_dir $your_checkpopint_path/chatglm-6b-lora-wx-$LR \
+    --overwrite_output_dir \
+    --max_source_length 1024 \
+    --max_target_length 512 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --max_steps 25000 \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --learning_rate $LR \
+    --lora_rank ${lora_rank} \
+    --trainable ${lora_trainable} \
+    --modules_to_save ${modules_to_save} \
+    --lora_dropout ${lora_dropout} \
---
+++ runs/train_gpt2_lora.sh
@@ -0,0 +1,40 @@
+lora_rank=8
+lora_trainable="c_attn"
+modules_to_save="null" # 保存除了lora层之外可训练的模块
+lora_dropout=0.1
+LR=2e-4
+model_name_or_path="gpt2-xl"
+#your_data_path="/data/wufan/llm/PromptCBLUE/datasets/PromptCBLUE/toy_examples"  # 填入数据集所在的文件夹路径
+your_data_path="/data/wufan/data/llm/AdvertiseGen"  # 填入数据集所在的文件夹路径
+your_checkpopint_path="./experiments/toy_examples_v1.0.0/"  # 填入用来存储模型的路径
+cache_dir=/data1/wufan/llm/cache
+deepspeed_config_file=./configs/deepspeed_config_zero2_offload.json
+# conda activate llm == > eight
+#    --overwrite_cache \
+torchrun \
+    --nnodes 1 \
+    --nproc_per_node 4 \
+    ft_clm/run_pt_clm.py \
+    --deepspeed ${deepspeed_config_file} \
+    --do_train \
+    --train_file $your_data_path/train.json \
+    --validation_file $your_data_path/dev.json \
+    --cache_dir ${cache_dir} \
+    --prompt_column input \
+    --response_column target \
+    --model_name_or_path $model_name_or_path \
+    --output_dir $your_checkpopint_path/gpt2-lora-$LR \
+    --overwrite_output_dir \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --max_steps 10000 \
+    --logging_steps 10 \
+    --save_steps 300 \
+    --block_size 512 \
+    --learning_rate $LR \
+    --lora_rank ${lora_rank} \
+    --trainable ${lora_trainable} \
+    --modules_to_save ${modules_to_save} \
+    --lora_dropout ${lora_dropout} \
+    --fp16
